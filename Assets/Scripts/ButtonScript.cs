f259b71b7fda757c6338dc4f1001e9ade5d572c1
KevinCoder <likeving8@gmail.com>
2025-09-27 13:37:43 -0500

    AAAAAAAAAAAA
    AAAAAAAAAAAAAAAAA

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool pressed = false;
+    Vector3 pos;
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(pressed);
+        if(pressed) {
+            pressButton();
+        }
+        else {
+            depressButton();
+        }
+    }
+    void OnTriggerStay2D(Collider2D collision) 
+    { 
+        Debug.Log("COLLIDED");
+        if (collision.gameObject.GetComponent<TimerScript>() != null) {
+            if(this.transform.parent.gameObject.GetComponent<TimerScript>().startingTime < collision.gameObject.GetComponent<TimerScript>().startingTime) {
+                pressed = true;
+            }
+            else {
+                pressed = false;
+            }
+            
+        } 
+        if (collision.gameObject.GetComponent<PlayerTimer>() != null) {
+            if(this.transform.parent.gameObject.GetComponent<TimerScript>().startingTime < collision.gameObject.GetComponent<TimerScript>().startingTime) {
+                pressed = true;
+            }
+            else {
+                pressed = false;
+            }
+        } 
+        
+    } 
+    void OnTriggerExit2D(Collider2D collision) 
+    { 
+        //Debug.Log("COLLIDED");
+        if (collision.gameObject.GetComponent<TimerScript>() != null) {
+           
+            pressed = false;
+        } 
+        if (collision.gameObject.GetComponent<PlayerTimer>() != null) {
+            
+            pressed = false;
+        } 
+        
+    } 
+    public void pressButton() {
+        transform.position = new Vector3(pos.x, pos.y - 0.1f, pos.z);
+    }
+    public void depressButton() {
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+    }
+}
