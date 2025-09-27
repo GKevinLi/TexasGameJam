bbcf9a83558beaf982b52a7a5f748177572ff169
KevinCoder <likeving8@gmail.com>
2025-09-27 12:49:27 -0500

    AAAAAAAAAAAAAAAAA
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -6,25 +6,25 @@ public class TimerScript : MonoBehaviour
 {
 
     public GameObject text;
-    private TMP_Text newText;
+    private GameObject newText;
     public int startingTime;
     public bool paused = false;
     private Coroutine timer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject temp = Instantiate(text, new Vector3(transform.position.x + (transform.position.x * 0.25f), transform.position.y + 1, transform.position.z), transform.rotation) as GameObject;
-        temp.SetActive(true);
-        newText = temp.GetComponent<TMP_Text>();
-        newText.text = startingTime + "";
+        newText = Instantiate(text, new Vector3(transform.position.x + (transform.position.x * 0.25f), transform.position.y + 1, transform.position.z), transform.rotation) as GameObject;
+        newText.SetActive(true);
+       
+        newText.GetComponent<TMP_Text>().text = startingTime + "";
         timer = StartCoroutine(secondCounter());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        newText.text = startingTime + "";
+        newText.transform.position = new Vector3(transform.position.x + 0.75f, transform.position.y + 1, transform.position.z);
+        newText.GetComponent<TMP_Text>().text = startingTime + "";
         if(startingTime <= 0) {
             Destroy(newText);
             Destroy(this.gameObject);
--- Assets/Scripts/ToolManager.cs
+++ Assets/Scripts/ToolManager.cs
@@ -8,10 +8,13 @@ public class ToolManager : MonoBehaviour
     private float scrollDelta;
     private int scrollVal;
     public GameObject[] tools;
+    public GameObject boxPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tools[0] = transform.Find("Add Tool").gameObject;
+        tools[1] = transform.Find("Remove Tool").gameObject;
+        tools[2] = transform.Find("Create Tool").gameObject;
     }
 
     // Update is called once per frame
@@ -22,11 +25,11 @@ public class ToolManager : MonoBehaviour
         // The Y component represents vertical scrolling
         scrollDelta = scrollValue.y;
         scrollVal += (int) scrollDelta;
-        scrollVal = scrollVal % 2;
+        scrollVal = scrollVal % 3;
         if(scrollVal == -1) {
-            scrollVal = 1;
+            scrollVal = 2;
         }
-        Debug.Log(scrollVal);
+        
         switchTools(scrollVal);
 
         if (Mouse.current.leftButton.wasPressedThisFrame)
@@ -36,6 +39,11 @@ public class ToolManager : MonoBehaviour
             }
             if(scrollVal == 1) {
                 gameObject.GetComponent<PlayerTimer>().removeTime();
+            }
+            if(scrollVal == 2) {
+                Vector3 vec = (Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
+	            Instantiate(boxPrefab, new Vector3(vec.x, vec.y, 10), transform.rotation);
+                gameObject.GetComponent<PlayerTimer>().startingTime -= 5;
             }
         }
 
